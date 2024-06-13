d20d59f974204169aea25a7cebe4d622c9c4aac9
Kerim200222 <115738542+Kerim200222@users.noreply.github.com>
2024-06-13 18:30:58 +0300

    Add files via upload

---
+++ src/banka_yonetim_sistemi/Banka_Yonetim_sistemi.java
@@ -0,0 +1,16 @@
+package banka_yonetim_sistemi;
+
+public class Banka_Yonetim_sistemi {
+
+        public static void main(String[] args) {
+         LoginFrame n = new LoginFrame();
+         n.setVisible(true);
+         AccountDetailsFrame m = new AccountDetailsFrame();
+         m.setVisible(true);
+         UserAddEditFrame nU = new UserAddEditFrame();
+         nU.setVisible(true);
+         UserManagementFrame nUM = new UserManagementFrame();
+         nUM.setVisible(true);
+    }
+    
+}
